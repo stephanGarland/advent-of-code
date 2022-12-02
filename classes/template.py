0d8df57956604964a9cbc445fc4b17d033c229ef
Stephan Garland <stephan.marc.garland@gmail.com>
2022-12-01 23:29:41 -0500

    chore: ran black

--- classes/template.py
+++ classes/template.py
@@ -2,12 +2,16 @@ import pathlib
 
 import aocd
 
+
 class AOCD:
     def __init__(self, file_path):
         self.file_path = file_path
         self.calling_file = pathlib.PurePath(self.file_path)
         self.text_file = f"{self.calling_file.with_stem(self.calling_file.stem.rstrip('ab_')).stem}.txt"
-        if not (pathlib.Path(self.text_file).is_file() and pathlib.Path(self.text_file).stat().st_size):
+        if not (
+            pathlib.Path(self.text_file).is_file()
+            and pathlib.Path(self.text_file).stat().st_size
+        ):
             self.save_puzzle(self.download_puzzle())
         self.puzzle = self.get_puzzle()
 
@@ -16,7 +20,7 @@ class AOCD:
 
     def get_puzzle(self):
         with open(self.text_file, "r") as f:
-            self.puzzle = f.readlines()
+            self.puzzle = f.read().splitlines()
         return self.puzzle
 
     def save_puzzle(self, data):
@@ -25,4 +29,3 @@ class AOCD:
 
     def submit_puzzle(self, answer):
         return aocd.submit(answer)
-
