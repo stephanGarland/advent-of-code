30bce44c9ede2d15a28d1bef56df8b664889e263
Stephan Garland <stephan.marc.garland@gmail.com>
2022-12-03 07:57:25 -0500

    chore: added boilerplate starter to everything

---
+++ 2022/09_b.py
@@ -0,0 +1,19 @@
+from classes.template import AOCD as Base
+
+
+class AOCD(Base):
+    pass
+
+
+class Solution:
+    """
+
+    """
+
+    def __init__(self):
+        self.aocd = AOCD(file_path=__file__)
+        self.data = [x if x else "" for x in self.aocd.puzzle]
+
+
+if __name__ == "__main__":
+    s = Solution()
