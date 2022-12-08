7e8e4a6b8848f489096823365a9eb88253db250d
Stephan Garland <stephan.marc.garland@gmail.com>
2022-12-08 00:19:00 -0500

    feat: added a unique sliding window finder

--- classes/utilities.py
+++ classes/utilities.py
@@ -1,9 +1,17 @@
 import itertools
+import more_itertools
 
 class Utilities:
 
     # https://stackoverflow.com/a/22045226/4221094
-    def make_group(self, iterator, group_size):
+    def make_groups(self, iterator, group_size: int) -> list[tuple]:
         it = iter(iterator)
         return list(iter(lambda: tuple(itertools.islice(it, group_size)), ()))
+
+    def find_first_unique_window(self, datastream, window_size: int, inclusive: bool = False) -> int:
+        for i, window in enumerate(datastream):
+            if len(set(window)) == window_size:
+                if inclusive:
+                    return i + window_size
+                return i
 
