82ab6807ee781bbf7a3cc37dfe9a4439d6027cd7
Stephan Garland <stephan.marc.garland@gmail.com>
2022-12-08 23:16:19 -0500

    feat: removed partial work from last year, will re-do later

--- 2021/classes/utilities.py
+++
@@ -1,6 +0,0 @@
-class Utilities:
-
-    def get_input() -> list:
-        with open("./input.txt", "r") as f:
-            ip = f.read().split("\n")
-        return ([x for x in ip if x], len(ip[0]))
