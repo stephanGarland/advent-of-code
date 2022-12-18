b3cfa02a3c70e4e6543af8a06fd7c684e456cc51
Stephan Garland <stephan.marc.garland@gmail.com>
2022-12-18 12:28:28 -0500

    chore: interim commit

--- 2022/08_b.py
+++ 2022/08_b.py
@@ -1,5 +1,10 @@
+from itertools import takewhile
+from math import prod
+from more_itertools import seekable
+
 from classes.template import AOCD as Base
 from classes.utilities import Utilities
+
 
 class AOCD(Base):
     pass
@@ -7,15 +12,97 @@ class AOCD(Base):
 
 class Solution:
     """
-
+    Given a matrix of integers representing the height of trees in a grid,
+    determine the highest scenic score for any tree in the grid, where the
+    scenic score is equal to the product of the number of trees visible
+    from each tree in each cardinal direction. For example, if a given tree
+    of height five can see 1 tree to its left and right, 2 trees to its
+    bottom, and 3 trees to its top, its score would be (1 * 1 * 2 * 3) == 6.
     """
 
     def __init__(self):
         self.aocd = AOCD(file_path=__file__)
-        self.data = [x for x in self.aocd.puzzle]
+        # self.data = [x for x in self.aocd.puzzle]
+        with open("08_example.txt", "r") as f:
+            self.data = f.read().splitlines()
         self.utilities = Utilities()
+
+    def make_slices(
+        self, coords: tuple, tree_matrix: list
+    ) -> dict[str, list[list[int]]]:
+        slices = {}
+        row, col = coords
+        top_slice = [int(tree[col]) for tree in tree_matrix][:row]
+        bottom_slice = [int(tree[col]) for tree in tree_matrix][row + 1 :]
+        left_slice = [int(x) for x in tree_matrix[row][:col]]
+        right_slice = [int(x) for x in tree_matrix[row][col + 1 :]]
+        return {
+            "top": top_slice,
+            "bottom": bottom_slice,
+            "left": left_slice,
+            "right": right_slice,
+        }
+
+    def loop_all_trees(self, tree_matrix: list) -> list:
+        visible = []
+        grid_length = len(tree_matrix)
+        for row in range(grid_length):
+            if row in [0, grid_length - 1]:
+                continue
+            for col in range(grid_length):
+                if col in [0, grid_length - 1]:
+                    continue
+                coords = (row, col)
+                tree_slices = self.make_slices(coords, tree_matrix)
+                visible_trees = self.find_visible(coords, tree_matrix, tree_slices)
+                visible.append(visible_trees)
+        return visible
+
+    def find_visible(
+        self, coords: tuple, tree_matrix: list, tree_slices: dict[str, list[list[int]]]
+    ) -> int:
+        visible = []
+        target_tree = int(tree_matrix[coords[0]][coords[1]])
+        msg = ""
+        for direction, trees in tree_slices.items():
+            visible_trees = 0
+            it = seekable(trees)
+            while bool(it):
+                msg = f"considering {trees} in direction {direction}: "
+                current_tree = next(it)
+                msg += f"current_tree: {current_tree}"
+                if current_tree < target_tree:
+                    msg += f" < {target_tree}, continue"
+                    visible_trees += 1
+                    print(it.peek(0))
+                if it.peek(0) >= target_tree:
+                    msg += f"peeking: {it.peek(0)} is >= {target_tree}, break"
+                    visible_trees += 1
+                    break
+                else:
+                    pass
+                    # msg += f"{it.peek(0)} is ? {target_tree}, freak out"
+                print(msg)
+                # if current_tree >= target_tree:
+                #    visible_trees += 1
+                #    break
+            visible.append(visible_trees)
+        return visible
+
+    def solve(self, tree_matrix: list) -> int:
+        grid_length = len(tree_matrix)
+        visible = self.loop_all_trees(tree_matrix)
+        return visible
 
 
 if __name__ == "__main__":
     s = Solution()
-
+    tree_matrix = s.data
+    for row in tree_matrix:
+        print(row)
+    ans = s.solve(tree_matrix)
+    groups = s.utilities.make_groups(ans, 3)
+    for g in groups:
+        print(g)
+    for row in ans:
+        print(prod(row))
