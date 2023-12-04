d16813fa7a980c9a6d45aae172f2f7672f67f8a2
Stephan Garland <stephan.marc.garland@gmail.com>
2023-12-04 14:18:17 -0500

    feat: day two complete

--- 2023/02_a.py
+++ 2023/02_a.py
@@ -8,14 +8,44 @@ class AOCD(Base):
 
 class Solution:
     """
-    Foobar
+    Given inputs of RGB cubes pulled from a bag, which games would have been
+    possible with only 12[R]13[G]14[B] cubes? Return the sum of the IDs of the
+    possible games (the ID is a monotonic 1-indexed number).
     """
+
+    LIMIT_RED = 12
+    LIMIT_GRN = 13
+    LIMIT_BLU = 14
 
     def __init__(self):
         self.aocd = AOCD(file_path=__file__)
         self.data = [x for x in self.aocd.puzzle]
         self.utilities = Utilities()
 
+    def game_parser(self, game_data: str) -> list:
+        game_rounds = []
+        game_data = game_data.split(":")[-1]
+        game_data = game_data.split(";")
+        game_data = [x.strip().split(", ") for x in game_data]
+        for game in game_data:
+            game_rounds.append({x.split(" ")[1]: int(x.split(" ")[0]) for x in game})
+        return game_rounds
+
+    def game_was_possible(self, rounds: list[dict[str, int]]) -> bool:
+        for rnd in rounds:
+            if (
+                rnd.get("red", 0) > Solution.LIMIT_RED
+                or rnd.get("green", 0) > Solution.LIMIT_GRN
+                or rnd.get("blue", 0) > Solution.LIMIT_BLU
+            ):
+                return False
+        return True
+
 
 if __name__ == "__main__":
     s = Solution()
+    games = []
+    for i, game in enumerate(s.data, start=1):
+        if s.game_was_possible(s.game_parser(game)):
+            games.append(i)
+    s.aocd.submit_puzzle(sum(games))
--- 2023/02_b.py
+++ 2023/02_b.py
@@ -1,21 +1,37 @@
-from classes.template import AOCD as Base
-from classes.utilities import Utilities
+from importlib import import_module
 
-
-class AOCD(Base):
-    pass
+PartOne = import_module("02_a", "Solution")
 
 
 class Solution:
     """
-    Foobar
+    Given inputs of RGB cubes pulled from a bag, what is the minimum number
+    of cubes to make that game possible? Find the product of the minimum
+    number of RGB cubes for each game (e.g. 4[R] * 2[G] * 3[B] = 24).
+    Return the sum of these products.
     """
 
     def __init__(self):
-        self.aocd = AOCD(file_path=__file__)
-        self.data = [x for x in self.aocd.puzzle]
-        self.utilities = Utilities()
+        self.part_one = PartOne.Solution()
+
+    def game_product(self, game_data: list) -> int:
+        red_needed, grn_needed, blu_needed = 0, 0, 0
+        for rnd in game_data:
+            red_needed = max(red_needed, rnd.get("red", 0))
+            grn_needed = max(grn_needed, rnd.get("green", 0))
+            blu_needed = max(blu_needed, rnd.get("blue", 0))
+        return red_needed * grn_needed * blu_needed
+
+    def solve(self, game: list):
+        products = []
+        products.append(self.game_product(self.part_one.game_parser(game)))
+        return sum(products)
 
 
 if __name__ == "__main__":
     s = Solution()
+    products = []
+    for game in s.part_one.data:
+        parsed = s.part_one.game_parser(game)
+        products.append(s.game_product(parsed))
+    s.part_one.aocd.submit_puzzle(sum(products))
